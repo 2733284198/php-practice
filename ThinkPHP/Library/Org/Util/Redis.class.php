9187ea77f9e03b1424ec6a5b3a5f30c0901c8bcf
Tinywan <756684177@qq.com>
2016-07-03 09:57:54 +0800

    添加一个Redis 单例模式

--- ThinkPHP/Library/Org/Util/Redis.class.php
+++ ThinkPHP/Library/Org/Util/Redis.class.php
@@ -12,8 +12,32 @@ namespace Org\Util;
 
 class Redis
 {
+    /**
+     * 保存类实例的静态成员变量
+     * @var
+     */
     private static $instance;
 
+    /**
+     * private标记的构造方法
+     * Redis constructor.
+     */
+    protected  function __construct(){
+        echo 'protected __Construnce';
+    }
+
+    /**
+     * 创建__clone方法防止对象被复制克隆
+     */
+    public function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    /**
+     * 单例方法,用于访问实例的公共的静态方法
+     * @return \Redis
+     */
     public static function init(){
         if(!(self::$instance instanceof \Redis)){
             self::$instance = new  \Redis();
@@ -21,4 +45,14 @@ class Redis
         }
         return self::$instance;
     }
+
+    /**
+     * null
+     * @return null
+     */
+    public function testRedis(){
+        echo 'Redis Success';
+        return null;
+    }
+
 }
