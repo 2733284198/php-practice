f871d07d4fd7ee0550e159b71b70b0109016aafc
Tinywan <756684177@qq.com>
2016-07-09 14:24:44 +0800

    默认的

--- Frontend/Home/Controller/JqueryController.class.php
+++ Frontend/Home/Controller/JqueryController.class.php
@@ -1,6 +1,11 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
+/**
+ * Class JqueryController
+ * @package Home\Controller
+ */
 class JqueryController extends Controller
 {
     public function index()
