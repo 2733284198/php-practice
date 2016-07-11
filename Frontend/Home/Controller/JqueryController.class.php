5300dbf7ecbc0c0c1ad2bd0808b19a1914d47760
Tinywan <756684177@qq.com>
2016-07-11 19:38:31 +0800

    ajaxStart 以及Ajax的错误码处理

--- Frontend/Home/Controller/JqueryController.class.php
+++ Frontend/Home/Controller/JqueryController.class.php
@@ -16,4 +16,44 @@ class JqueryController extends Controller
     public function demo1(){
         $this->display();
     }
+
+    public function event()
+    {
+        $this->display();
+    }
+
+    public function loading(){
+        if($_GET[id] == 'hello'){
+            echo 'hello';
+        }else{
+            echo 'XXXXXXXXXXXXXX';
+        }
+    }
+
+    public function ajax1()
+    {
+        $this->display();
+    }
+
+    /**
+     * 全局事件
+     */
+    public function ajaxStart()
+    {
+        $this->display();
+    }
+
+    public function startData()
+    {
+        sleep(10);
+        if($_POST['username'] == 'wanwan'){
+            echo 'wanwan ning hao';
+        }else{
+            echo 'XXXXXXXXXXXXXX';
+        }
+    }
+
+    public function jsonp(){
+        $this->display();
+    }
 }
