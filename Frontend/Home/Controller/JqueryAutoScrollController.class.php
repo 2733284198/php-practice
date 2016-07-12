c83936b2ce38008e410208235c40a9b2f08bfa58
Tinywan <756684177@qq.com>
2016-07-12 20:25:13 +0800

    Jquery 自定义滚动条

---
+++ Frontend/Home/Controller/JqueryAutoScrollController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+/**
+ * Class JqueryController
+ * @package Home\Controller
+ */
+class JqueryAutoScrollController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+}
