b14dc9e4e4e098bf8a062abfa6d646b4560f052c
Tinywan <756684177@qq.com>
2016-07-12 10:30:03 +0800

    使用Redis 做缓存的实际案例

--- Frontend/Home/Controller/ApiController.class.php
+++ Frontend/Home/Controller/ApiController.class.php
@@ -10,9 +10,12 @@ use Think\Controller;
 class ApiController extends Controller
 {
     const API_LIVE = 'http://www.amailive.com/api/getLive';
+
     const API_VIDEO = 'http://www.amailive.com/api/getVideo';
+
     const API_VIDEO_INFO = 'http://www.amailive.com/api/videoInfo';
-    const CACHE_KEY = 'LIVE_INFO';
+
+    const CACHE_KEY = 'LIVE_INFO'; //你就当做表名吧！
 
     /**
      * @var array
@@ -47,8 +50,8 @@ class ApiController extends Controller
         $redis = new \Redis();
         $redis->connect('127.0.0.1', 6379);
         $redis->auth('');
-        //这里在操作Redis之前最好判断Reids是否连接服务器成功,可以通过一个Ping命令测试
-        if ($redis->ping() == 'PONG') {
+        //这里在操作Redis之前最好判断Reids是否连接服务器成功,可以通过一个Ping命令测试,返回一个“+PONG”字符串
+        if ($redis->ping() == '+PONG') {
             // 获取该活动的缓存数据
             $liveInfo = $redis->hGet(self::CACHE_KEY, $liveId);
             $resultData = [];
@@ -75,12 +78,56 @@ class ApiController extends Controller
                 }
                 $resultData = $result;
             }
-        } else {
+        }
+        else // Redis数据库连接失败，则直接读取接口数据即可
+        {
             $response = curlPost(self::API_LIVE, self::$liveParams);
             $result = json_decode($response, true);
             $resultData = $result;
         }
         return $resultData;
+    }
+
+    /**
+     * 蝉知中使用Redis 做缓存的实际案例
+     * @param string $deviceId
+     */
+    public function liveRedis($deviceId = '2853')
+    {
+        $redis = new \Redis();
+        $redis->connect('127.0.0.1',6379);
+        $redis->auth('mastertestpassword');
+        $resultData = [];
+        $liveInfo = json_decode($this->online->postCurl(self::LIVE_URL, self::$liveData), true);
+
+        if($redis->ping() == '+PONG'){
+            $resultData['errcode'] = 'Redis 数据库连接成功';
+            $getResult = $redis->hGet(self::CACHE_KEY,$deviceId);
+            if($getResult){
+                $resultData = json_decode($getResult,true);
+                $resultData['DataSource'] = '来自Redis数据库中的数据';
+                $resultData['errcode'] = 'OK';
+            }else{
+                $liveInfo = json_decode($this->online->postCurl(self::LIVE_URL, self::$liveData), true);
+                if($liveInfo['errcode'] == 200){
+                    foreach($liveInfo['dataList'] as $key=>$values)
+                    {
+                        //根据设别号存储数据
+                        $redis->hset(self::CACHE_KEY,$values['deviceId'],json_encode($values));
+                    }
+                    $resultData[] = $liveInfo['dataList'];
+                    $redis->expire(self::CACHE_KEY,60);
+                    $resultData['DataSource'] = '来自API接口数据';
+                    $resultData['errcode'] = 'OK';
+                }else{
+                    $resultData['DataSource'] = '接口数据获取失败';
+                    $resultData['errcode'] = 'Fail';
+                }
+            }
+        }else{
+            $resultData['errcode'] = 'Redis 数据库连接失败';
+        }
+        var_dump($resultData);
     }
 
     public function getVideo()
