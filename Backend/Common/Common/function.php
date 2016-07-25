9b7b6c3695a23ba39d601e0d98005fceeb25de2f
Tinywan <756684177@qq.com>
2016-07-25 17:08:44 +0800

    PHP发送Post数据一个函数，同时携带Http状态码返回

--- Backend/Common/Common/function.php
+++ Backend/Common/Common/function.php
@@ -108,4 +108,53 @@ function Get($url)
     }
     return $file_contents;
 }
+
+/**
+ * PHP发送Post数据
+ *
+ * @param string $url 请求url
+ * @param array/string $params 发送的参数
+ * @return array
+ */
+function http_post_data($url, $params = array())
+{
+    //在没有需要上传文件的情况下，尽量对 post 提交的数据进行 http_build_query 处理，然后再发送出去，能实现更好的兼容性，更小的请求数据包。
+    if (is_array($params))
+    {
+        $params = http_build_query($params, null, '&');
+    }
+
+    //初始化一个curl会话，curl_init()函数唯一的一个参数是可选的，表示一个url地址
+    $curl = curl_init();
+
+    //这是你想用PHP取回的URL地址
+    curl_setopt($curl, CURLOPT_URL, $url);
+
+    //如果想把一个头包含在输出中，设置这个选项为一个非零值。
+    curl_setopt($curl, CURLOPT_HEADER, false);
+
+    //如果成功只将结果返回，不自动输出任何内容。如果失败返回FALSE
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    //如果你不想在输出中包含body部分，设置这个选项为一个非零值。
+    curl_setopt($curl, CURLOPT_NOBODY, true);
+
+    //如果你想PHP去做一个正规的HTTP POST，设置这个选项为一个非零值。
+    //这个POST是普通的 application/x-www-from-urlencoded 类型，多数被HTML表单使用
+    curl_setopt($curl, CURLOPT_POST, true);
+
+    // 传递一个作为HTTP “POST”操作的所有数据的字符串
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+
+    //执行一个curl会话
+    $response = curl_exec($curl);
+
+    //获取一个Http服务器状态吗
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    //关闭一个curl会话
+    curl_close($curl);
+
+    return array($httpCode, $response);
+}
 ?>
