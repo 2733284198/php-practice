acc9144f145e20e78c382c57c537017d513bbb79
Tinywan <756684177@qq.com>
2016-07-28 09:12:14 +0800

    尝试去调用一个Category模块下面Index控制器的Test()的方法

--- Backend/Home/Controller/IndexController.class.php
+++ Backend/Home/Controller/IndexController.class.php
@@ -6,4 +6,38 @@ class IndexController extends Controller {
     {
         echo 'Backend';
     }
+
+    /**
+     * 通过 A 方法跨模块调用操作
+     * 尝试去调用一个Category模块下面Index控制器的Test()的方法
+     * A()方法
+     * ***建议：如果要使用其他模块内的多个方法，那么建议使用 A 方法，通过对象的方式调用该模块的不同方法，避免多次实例化对象。
+     */
+    public function aGetModule(){
+        $category = A('Category/Index');
+        $category->test();
+    }
+
+    /**
+     * 通过 R 方法跨模块调用操作
+     * 尝试去调用一个Category模块下面Index控制器的Test()的方法
+     * R()方法
+     * ***建议：如果只需要使用其他模块内的其中一个方法，那么无疑 R 方法是最简洁的
+     */
+    public function rGetModule(){
+        $category = R('Category/Index/test');
+        echo $category;
+    }
+
+    /**
+     * 通过 R 方法跨模块调用操作
+     * 尝试去调用一个Category模块下面Index控制器的Test()的方法
+     * R()方法
+     * 建议：如果只需要使用其他模块内的其中一个方法，那么无疑 R 方法是最简洁的
+     */
+    public function aGetAppModule(){
+        $category = A('ThinkPhpStudy://Frontend/Home/Index/index');
+        echo $category;
+        echo '111111111';
+    }
 }
