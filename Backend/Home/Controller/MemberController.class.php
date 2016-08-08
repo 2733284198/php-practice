f29adfd2be2525ce23ff66fab8451a5ea784a1c4
Tinywan <756684177@qq.com>
2016-08-08 23:50:37 +0800

    密码修改成功

--- Backend/Home/Controller/MemberController.class.php
+++ Backend/Home/Controller/MemberController.class.php
@@ -91,23 +91,32 @@ class MemberController extends BaseController
     {
         $username = I('post.username');
         $password = I('post.password');
+        $oldPassword = I('post.oldpassword');
         $User = M('User');
         $conditions = array('username' => ':username');
         $result = $User->where($conditions)->bind(':username', $username)->find();
 
         $User->password = md5($password);
         $User->where(array('id' => $result->id));
-        if ($User->save() == false) {
-            return $this->error("密码修改失败", U('Member/memberCenter'));
+        // 检查旧密码是否正确
+        if($result['password'] == md5($oldPassword)){
+            $User->password = md5($password);
+            $User->where(array('user_id'=>$result['user_id']));
+            if($User->save() == false){
+                return $this->error("密码修改失败",U('Member/memberCenter'));
+            }else{
+                //==============释放用户的session所有资源，需要顺序执行如下代码=============
+                //1.删除某个session的值使用
+                session('username',NULL);
+                //2.释放当前在内存中已经创建的所有$_SESSION变量，但不删除session文件以及不释放对应的sessionid
+                session_unset();
+                //3.删除当前用户对应的session文件以及释放sessionid，内存中的$_SESSION变量内容依然保留
+                session_destroy();
+                return $this->success('密码修改成功',U('Login/index'));
+            }
+        }else{
+            return $this->error("旧密码错误",U('Member/memberCenter'));
         }
-        //==============释放用户的session所有资源，需要顺序执行如下代码=============
-        //1.删除某个session的值使用
-        session('username',NULL);
-        //2.释放当前在内存中已经创建的所有$_SESSION变量，但不删除session文件以及不释放对应的sessionid
-        session_unset();
-        //3.删除当前用户对应的session文件以及释放sessionid，内存中的$_SESSION变量内容依然保留
-        session_destroy();
-        return $this->success('密码修改成功', U('Login/index'));
 
     }
 
