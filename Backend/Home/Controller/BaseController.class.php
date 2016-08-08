325068a4176d88a1e0913f489ea659417ef9a0a7
756684177@qq.com <756684177@qq.com>
2016-08-08 08:51:37 +0800

    判断一个session是否设置：session('?name'); 相当于：isset($_SESSION['name']);

--- Backend/Home/Controller/BaseController.class.php
+++ Backend/Home/Controller/BaseController.class.php
@@ -7,6 +7,8 @@ class BaseController extends Controller
 {
     /**
      * ThikPHP自动运行方法，每一次，都会自动运行这个方法
+     * 要判断一个session值是否已经设置，可以使用
+       session('?name'); 相当于：isset($_SESSION['name']);
      */
     public function _initialize(){
         if(!isset($_SESSION['username']))
