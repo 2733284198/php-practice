d409ed5cdc2ca9f52a6a02e6b600efd5262762e0
Tinywan <756684177@qq.com>
2016-08-08 23:26:30 +0800

    替换了一个新的模板页面

--- Backend/Home/Controller/BaseController.class.php
+++ Backend/Home/Controller/BaseController.class.php
@@ -5,15 +5,15 @@ use Think\Controller;
 
 class BaseController extends Controller
 {
-    /**
-     * ThikPHP自动运行方法，每一次，都会自动运行这个方法
-     * 要判断一个session值是否已经设置，可以使用
-       session('?name'); 相当于：isset($_SESSION['name']);
-     */
-    public function _initialize(){
-        if(!session('?username'))
-        {
-            return $this->redirect('Login/index');
-        }
-    }
+//    /**
+//     * ThikPHP自动运行方法，每一次，都会自动运行这个方法
+//     * 要判断一个session值是否已经设置，可以使用
+//       session('?name'); 相当于：isset($_SESSION['name']);
+//     */
+//    public function _initialize(){
+//        if(!session('?username'))
+//        {
+//            return $this->redirect('Login/index');
+//        }
+//    }
 }
