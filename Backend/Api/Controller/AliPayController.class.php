a1fdd21a903bdbebab3a1b4a8e7b127d4a433233
Tinywan <756684177@qq.com>
2016-09-16 13:20:48 +0800

    支付宝即时到账接口开发API

--- Backend/Api/Controller/AliPayController.class.php
+++ Backend/Api/Controller/AliPayController.class.php
@@ -13,6 +13,9 @@ use Think\Controller;
 class AliPayController extends Controller
 {
 
+    /**
+     * 商品显示列表
+     */
     public function index()
     {
         $map['id'] = 3;
@@ -21,13 +24,59 @@ class AliPayController extends Controller
         $this->display();
     }
 
+    /**
+     * 订单异步提交数据
+     */
     public function payTo()
     {
-        $data['subject'] = $_POST['subject'];
-        $data['out_trade_no'] = $_POST['out_trade_no'];
-        $data['total_fee'] = $_POST['total_fee'];
+        $data['subject'] = $_POST['subject'];  //商品名称
+        $data['out_trade_no'] = $_POST['out_trade_no']; //商户网站唯一订单号
+        $data['total_fee'] = $_POST['total_fee'];   //交易金额
         $res = D('Common/Pay')->alipay($data);
         $this->ajaxReturn($res);
+    }
+
+    /**
+     * 退款
+     */
+    public function refund(){
+        header("Content-type:text/html;charset=utf-8");
+        $data['batch_no'] = date('YmdHis').'MZ01';
+        $data['batch_num'] = 1;
+        $data['detail_data'] = '2016091621001004480242264394^0.01^协商退款01';
+        $res = D('Common/Refund')->refund($data);
+        //var_dump($res); 这个用在测试的时候，如果打印的是一个表单的话表示错误
+        echo($res);
+    }
+
+    //同步通知
+    public function return_url(){
+        $out_trade_no = $_GET['out_trade_no'];
+        $trade_no = $_GET['trade_no'];
+        $trade_status = $_GET['trade_status'];
+        if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
+            $map['out_trade_no'] = $out_trade_no;
+            $data['trade_no'] = $trade_no;
+            $res = M('alipay')->where($map)->save($data);
+            if($res !== false){
+                echo 'success';
+            }
+        }
+    }
+
+    //异步通知
+    public function notify_url(){
+        $out_trade_no = $_POST['out_trade_no'];
+        $trade_no = $_POST['trade_no'];
+        $trade_status = $_POST['trade_status'];
+        if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
+            $map['out_trade_no'] = $out_trade_no;
+            $data['trade_no'] = $trade_no;
+            $res = M('alipay')->where($map)->save($data);
+            if($res !== false){
+                echo 'success';
+            }
+        }
     }
 
 }
--- Backend/Api/Controller/NotifyController.class.php
+++ Backend/Api/Controller/NotifyController.class.php
@@ -1,34 +1,52 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Tinywan
+ * Date: 2016/9/16
+ * Time: 10:44
+ */
 namespace Api\Controller;
 use Think\Controller;
 class NotifyController extends Controller {
-	//同步通知
-	public function return_url(){
-		$out_trade_no = $_GET['out_trade_no'];
-		$trade_no = $_GET['trade_no'];
-		$trade_status = $_GET['trade_status'];
-		if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
-			$map['out_trade_no'] = $out_trade_no;
-			$data['trade_no'] = $trade_no;
-			$res = M('alipay')->where($map)->save($data);
-			if($res !== false){
-				echo 'success';
-			}
-		}
-	}
-	
-	//异步通知
-	public function notify_url(){
-		$out_trade_no = $_POST['out_trade_no'];
-		$trade_no = $_POST['trade_no'];
-		$trade_status = $_POST['trade_status'];
-		if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
-			$map['out_trade_no'] = $out_trade_no;
-			$data['trade_no'] = $trade_no;
-			$res = M('alipay')->where($map)->save($data);
-			if($res !== false){
-				echo 'success';
-			}
-		}
-	}
+    //同步通知
+    public function return_url(){
+        $verify = D('Common/Pay')->verifyReturn();
+        if($verify){
+            $out_trade_no = $_GET['out_trade_no'];
+            $trade_no = $_GET['trade_no'];
+            $trade_status = $_GET['trade_status'];
+            if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
+                $map['out_trade_no'] = $out_trade_no;
+                $data['trade_no'] = $trade_no;
+                $res = M('alipay')->where($map)->save($data);
+                if($res !== false){
+                    echo 'success,return_url'.$res;
+                }
+            }
+        }
+
+    }
+
+    //异步通知
+    public function notify_url(){
+        $verify = D('Common/Pay')->verifyNotify();
+        if($verify){
+            $out_trade_no = $_POST['out_trade_no'];
+            $trade_no = $_POST['trade_no'];
+            $trade_status = $_POST['trade_status'];
+            if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
+                $map['out_trade_no'] = $out_trade_no;
+                $data['trade_no'] = $trade_no;
+                $res = M('alipay')->where($map)->save($data);
+                if($res !== false){
+                    echo 'success notify_url--$res';
+                }
+            }
+        }
+    }
+
+    public function test(){
+        $map['id'] = 4 ;
+        var_dump($map);
+    }
 }
