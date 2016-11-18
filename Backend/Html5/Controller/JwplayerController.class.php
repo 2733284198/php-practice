51cc355cb315eb75f7ebccb2fee984db4af0352a
756684177@qq.com <756684177@qq.com>
2016-11-18 11:57:10 +0800

    Jwplayer播放器播

---
+++ Backend/Html5/Controller/JwplayerController.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Html5\Controller;
+
+use Think\Controller;
+
+class JwplayerController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function testtpl()
+    {
+        $this->display();
+    }
+
+    public function setTimeOut()
+    {
+        $this->display();
+    }
+
+}
