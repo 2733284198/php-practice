3e876d1388f7d0820b4f5e39fe1cb73fc29718a4
756684177@qq.com <756684177@qq.com>
2016-12-30 17:13:47 +0800

    用户自定义m3u8

--- Backend/Home/Controller/WebRTCController.class.php
+++ Backend/Home/Controller/WebRTCController.class.php
@@ -22,4 +22,24 @@ class WebRTCController extends BaseController
     {
         $this->display('WebRTC/getusermedia');
     }
+
+    public function RecordRTC()
+    {
+        $this->display('WebRTC/recordrtc');
+    }
+
+    public function uploadAudio()
+    {
+        $upload = __ROOT__ . '/Uploads/RecordRTC/';
+        foreach (array('video', 'audio') as $type) {
+            if (isset($_FILES["${type}-blob"])) {
+                $fileName = $_POST["${type}-filename"];
+                $uploadDirectory = $upload . $fileName;
+                if (!move_uploaded_file($_FILES["${type}-blob"]["tmp_name"], $uploadDirectory)) {
+                    echo(" problem moving uploaded file");
+                }
+                echo($uploadDirectory);
+            }
+        }
+    }
 }
