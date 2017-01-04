152ed5a1e76f6d18c02bb5d2663daa49aae51841
756684177@qq.com <756684177@qq.com>
2017-01-04 17:45:39 +0800

    php 操作FFmpeg的基本用法

--- Backend/Home/Controller/FFmpegController.class.php
+++ Backend/Home/Controller/FFmpegController.class.php
@@ -16,7 +16,7 @@ class FFmpegController extends Controller
         echo 'FFmpeg';
     }
 
-    //Basic Usage
+    //Basic Usageerr
     public function createFFmpeg()
     {
         $path = 'http://' . $_SERVER['HTTP_HOST'] . __ROOT__ . '/Uploads/FFmpegVideo/';
