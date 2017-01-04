67935ed9e5a446c7528f4713174146be14569e78
756684177@qq.com <756684177@qq.com>
2017-01-04 17:43:47 +0800

    php 操作FFmpeg的基本用法

--- Backend/Home/Controller/FFmpegController.class.php
+++ Backend/Home/Controller/FFmpegController.class.php
@@ -2,23 +2,42 @@
 namespace Home\Controller;
 
 use FFMpeg\FFMpeg;
+use FFMpeg\Coordinate;
+use FFMpeg\Format;
 use Think\Controller;
+
 require 'vendor/autoload.php';
 
 class FFmpegController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         echo 'FFmpeg';
     }
+
     //Basic Usage
-    public function createFFmpeg(){
-        $ffmpeg =   FFMpeg::create();
-        var_dump($ffmpeg);
+    public function createFFmpeg()
+    {
+        $path = 'http://' . $_SERVER['HTTP_HOST'] . __ROOT__ . '/Uploads/FFmpegVideo/';
+        $ffmpeg = FFMpeg::create();
+        $video = $ffmpeg->open($path.'out.mpg');
+        $video->filters()
+            ->resize(new Coordinate\Dimension(320, 240))
+            ->synchronize();
+        $video->frame(Coordinate\TimeCode::fromSeconds(10))
+            ->save($path.'frame.jpg');
+        $video->save(new Format\Video\X264(),'export-x264123.mp4');
+        var_dump($video);
     }
 
-
-
+    //Basic Usage
+    public function movie()
+    {
+        $videoPath = 'http://' . $_SERVER['HTTP_HOST'] . __ROOT__ . '/Uploads/FFmpegVideo/out.mpg';
+        $movie = new \ffmpeg_movie($videoPath,true);
+        var_dump($movie->getDuration());
+    }
 
 
 }
