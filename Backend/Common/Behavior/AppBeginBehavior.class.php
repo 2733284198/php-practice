2a3a4edbbb704a72ab8c997b0b4a6c3d5c77b0a0
756684177@qq.com <756684177@qq.com>
2017-01-17 09:27:40 +0800

    添加Behavior 行为扩展

---
+++ Backend/Common/Behavior/AppBeginBehavior.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/** .-------------------------------------------------------------------
+ * |  Github: https://github.com/Tinywan
+ * |  Blog: http://www.cnblogs.com/Tinywan
+ * |-------------------------------------------------------------------
+ * |  Author: Tinywan
+ * |  Time : 9:26
+ * |  Mail: Overcome.wan@Gmail.com
+ * |  Created by PhpStorm.
+ * '-------------------------------------------------------------------*/
+class AppBeginBehavior
+{
+
+}
