5cb48db702aeccd346a8aa7e9cd354a5ea9d8b70
756684177@qq.com <756684177@qq.com>
2017-01-17 10:01:31 +0800

    行为类建议继承\Think\Behavior

--- Backend/Common/Behaviors/AppBeginBehavior.class.php
+++ Backend/Common/Behaviors/AppBeginBehavior.class.php
@@ -5,11 +5,17 @@
  * |  Blog: http://www.cnblogs.com/Tinywan
  * |-------------------------------------------------------------------
  * |  Author: Tinywan
- * |  Time : 9:26
+ * |  Date: 2017/1/17
+ * |  Time: 9:58
  * |  Mail: Overcome.wan@Gmail.com
  * |  Created by PhpStorm.
  * '-------------------------------------------------------------------*/
-class AppBeginBehavior
+//行为类建议继承\Think\Behavior，必须实现run(&$param)方法，行为是通过这个方法执行的。
+class AppBeginBehavior extends \Think\Behavior
 {
+    //行为执行入口
+    public function run(&$params)
+    {
 
+    }
 }
