8646f49f1a8ddfae5fd32a8961ed748eb16f1f1a
756684177@qq.com <756684177@qq.com>
2017-01-17 10:11:48 +0800

    行为类建议继承\Think\Behavior

--- Backend/Common/Conf/tags.php
+++ Backend/Common/Conf/tags.php
@@ -1,26 +1,14 @@
 <?php
-
 /** .-------------------------------------------------------------------
  * |  Github: https://github.com/Tinywan
  * |  Blog: http://www.cnblogs.com/Tinywan
  * |-------------------------------------------------------------------
  * |  Author: Tinywan
  * |  Date: 2017/1/17
- * |  Time: 9:58
+ * |  Time: 10:04
  * |  Mail: Overcome.wan@Gmail.com
  * |  Created by PhpStorm.
  * '-------------------------------------------------------------------*/
-namespace Common\Behaviors;
-//行为类建议继承\Think\Behavior，必须实现run(&$param)方法，行为是通过这个方法执行的。
-class AppBeginBehavior extends \Think\Behavior
-{
-    //行为执行入口
-    public function run(&$params)
-    {
-        $this->test();
-    }
-
-    protected function test(){
-        return '行为类建议继承\Think\Behavior';
-    }
-}
+return [
+    'app_begin' => ['Common\Behaviors\AppBeginBehavior']
+];
