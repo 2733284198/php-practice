d66005d900f911157b09729a9ac43e9e7963348b
756684177@qq.com <756684177@qq.com>
2017-01-18 10:40:46 +0800

    ConfigController

---
+++ Backend/WeChat/Controller/ConfigController.class.php
@@ -0,0 +1,24 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Github: https://github.com/Tinywan
+ * |  Blog: http://www.cnblogs.com/Tinywan
+ * |-------------------------------------------------------------------
+ * |  Author: Tinywan
+ * |  Date: 2017/1/18
+ * |  Time: 10:39
+ * |  Mail: Overcome.wan@Gmail.com
+ * |  Created by PhpStorm.
+ * '-------------------------------------------------------------------*/
+
+namespace WeChat\Controller;
+
+
+use Common\Controller\BaseController;
+
+class ConfigController extends BaseController
+{
+    //配置信息
+    public function set(){
+        echo 'Wechat';
+    }
+}
