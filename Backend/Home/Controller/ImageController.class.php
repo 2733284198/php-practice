0db96a86b167e4073fd80f80bc145931013885d3
Tinywan <756684177@qq.com>
2017-01-20 17:41:44 +0800

    Save image in filesystem

--- Backend/Home/Controller/ImageController.class.php
+++ Backend/Home/Controller/ImageController.class.php
@@ -21,11 +21,35 @@ class ImageController extends Controller
         echo 'composer require intervention/image';
     }
 
-    public function init()
+    /**
+     * Reading Images
+     */
+    public function ReadingImages()
     {
         $inputPath = 'http://' . $_SERVER['HTTP_HOST'] . __ROOT__ . '/Uploads/Images/20130925095904723.jpg'; //项目路径
-        $outputPath =  __ROOT__ . '/Uploads/Images/12.jpg'; //项目路径
         //设置文件大小,同时保存文件为png格式
-        $image = Image::make($inputPath)->resize(320,240)->save('123.png');
+        Image::make($inputPath)->resize(320,240)->save('123.png');
+    }
+
+    /**
+     * Creating Images,Creating new images with background color
+     */
+    public function CreatingImages()
+    {
+        $img = Image::canvas(800, 600, '#ccc');
+        var_dump($img);
+    }
+    
+    /**
+     *  Save image in filesystem
+     */
+    public function  SaveImage()
+    {
+        $inputPath = 'http://' . $_SERVER['HTTP_HOST'] . __ROOT__ . '/Uploads/Images/20130925095904723.jpg'; //项目路径
+        $image = Image::make($inputPath)->resize(320,240);
+        // save file as png with medium quality
+        $image->save('Uploads/Images/bar123.png',60);
+        // save the same file as jpeg with default quality
+        $image->save('Uploads/Images/bar123.jpg');
     }
 }
