4bac6a151b0c6a927c9ba4c84e0859dfd572aa10
Tinywan <756684177@qq.com>
2017-02-04 13:08:42 +0800

    遇到的问题：刚开始在这里一直上传一个文件夹，始终上传不了，检查到最后结果是文件路径多一个‘/’,这个应该是官方的Debug吧！哈哈

--- Backend/Home/Controller/OSSController.class.php
+++ Backend/Home/Controller/OSSController.class.php
@@ -44,7 +44,7 @@ class OSSController extends Controller
     public function uploadFile()
     {
         $bucket = C("OSS_CONFIG.bucket");
-        $object = 'Uploads/BaiduApp/BaiduApp_v1.0.zip'; //！！！注意：这里要强调的是这个文件路径前面没有‘/’
+        $object = 'Uploads/BaiduApp/mini_BaiduApp_v1.0.jpg'; //！！！注意：这里要强调的是这个文件路径前面没有‘/’
         $file = './' . $object;  //这个才是文件在本地的真实路径，也是就是你要上传的文件信息
         $oss = OssInstance::Instance();
         try {
