3645c978399a8d2366a43302485e34aa1c5d35be
Tinywan <756684177@qq.com>
2017-02-06 13:10:01 +0800

    Faker生成中文的测试数据

--- Backend/Home/Controller/FakerController.class.php
+++ Backend/Home/Controller/FakerController.class.php
@@ -19,9 +19,13 @@ class FakerController
 {
     public function Basic_Usage()
     {
-        $faker = Factory::create();
-        echo $faker->name."<br/>";
-        echo $faker->address."<br/>";
-        echo $faker->text;
+        $faker = Factory::create($locale = 'zh_CN');
+        echo $faker->name . "<br/>";
+        echo $faker->address . "<br/>";
+        for ($i = 0; $i < 10; $i++) {
+            echo $faker->name, "<br/>";
+        }
+        //适用于抛出异常的字符串格式化输出
+        //throw new \InvalidArgumentException(sprintf('Unable to find provider "%s" with locale "%s"', $faker->name, $faker->address));
     }
 }
