810b9d02ae9432444d4d4c0e34e58e62d34a7c34
Tinywan <756684177@qq.com>
2017-02-06 17:40:37 +0800

    一个简单的APi接口签名方法SignEncryptController

--- Backend/Home/Controller/SignEncryptController.class.php
+++ Backend/Home/Controller/SignEncryptController.class.php
@@ -16,20 +16,17 @@ namespace Home\Controller;
 class SignEncryptController
 {
     /**
-     * APi自定义签名方法
+     * APi自定义签名方法（PS：乐视云的签名方法也是这样的哦！！！！！！！）
      * appId会在请求中作为一个应用标识参与接口请求的参数传递，appSecret 将作为唯一不需要参数传递,但是作为加密的参数
      */
-    public function getSign()
+    public function getSign($appId,$method)
     {
-        $appId = 13669361192;
+        //HP服务端先要给开发者（APP）分配一个appId与appSecret (正常情况下，开发者要到服务提供商的官网申请)，作为客户端，需要保留好官方颁发的appid & appsecret
         $appSecret = 'r5e2t85tyu142u665698fzu';
+        //移动客户端，需要请求服务列表，包括方法哦!
         $allParam = [
             'appid' => $appId,
-            'method' => 'createPushFlowAddress',
-            'serviceArea' => 'A',
-            'authKeyStatus' => 0,
-            'appName' => 'live',
-            'domainName' => 'tinywan.amai8.com',
+            'method' => $method,
         ];
         // 1. 对加密数组进行字典排序
         foreach ($allParam as $key => $value) {
@@ -46,7 +43,42 @@ class SignEncryptController
         $finalStr = $str . $appSecret;
         //4. 通过sha1加密,转化为大写大写获得签名
         $sign = strtoupper(sha1($finalStr));
-        echo $sign;
+        return $sign;
+    }
+
+    /**
+     * 服务端验证签名方式
+     * @param $appId
+     * @param $method
+     * @return string
+     */
+    public function serviceSign($appId,$method)
+    {
+        $appSecret = 'r5e2t85tyu142u665698fzu';
+        $allParam = [
+            'appid' => $appId,
+            'method' => $method,
+//            'serviceArea' => 'A',
+//            'authKeyStatus' => 0,
+//            'appName' => 'live',
+//            'domainName' => 'tinywan.amai8.com',
+        ];
+        // 1. 对加密数组进行字典排序
+        foreach ($allParam as $key => $value) {
+            $sortParam[$key] = $key;
+        }
+        // 2. 字典排序的作用就是防止因为参数顺序不一致而导致下面拼接加密不同
+        sort($sortParam);
+        // 3. 将Key和Value拼接
+        $str = "";
+        foreach ($sortParam as $k => $v) {
+            $str = $str . $sortParam[$k] . $allParam[$v];
+        }
+        //3.将appSecret作为拼接字符串的后缀,形成最后的字符串
+        $finalStr = $str . $appSecret;
+        //4. 通过sha1加密,转化为大写大写获得签名
+        $sign = strtoupper(sha1($finalStr));
+        return $sign;
     }
 
     /**
@@ -63,5 +95,49 @@ class SignEncryptController
         $appsecret = 'r5e2t85tyu142u665698fzu';
         $sign = sha1("appName{$appName}appid{$appid}authKeyStatus{$authKeyStatus}domainName{$domainName}method{$method}serviceArea{$serviceArea}{$appsecret}");
         var_dump(strtoupper($sign));
+    }
+
+    /**
+     * 测试请求方法
+     */
+    public function testRequest()
+    {
+        $appId = 13669361192;
+        $domainName = 'tinywan.amai8.com';
+        $appName = 'live';
+        $appSecret = sha1('http://sewise.amai8.com/');
+        $allParam = [
+            'appid' => $appId,
+            'domainName' => $domainName,
+            'appName' => $appName,
+        ];
+        // 1. 对加密数组进行字典排序
+        foreach ($allParam as $key => $value) {
+            $sortParam[$key] = $key;
+        }
+        // 2. 字典排序的作用就是防止因为参数顺序不一致而导致下面拼接加密不同
+        sort($sortParam);
+        // 3. 将Key和Value拼接
+        $str = "";
+        foreach ($sortParam as $k => $v) {
+            $str = $str . $sortParam[$k] . $allParam[$v];
+        }
+        //3.将appSecret作为拼接字符串的后缀,形成最后的字符串
+        $finalStr = $str . $appSecret;
+        echo $finalStr;
+        //4. 通过sha1加密,转化为大写大写获得签名
+        $sign = strtoupper(sha1($finalStr));
+        homePrint($sign);
+        die;
+        $url = "http://sewise.amai8.com/openapi/createPushFlowAddress?AppId=".$appId."&AppName=".$appName."&DomainName=".$domainName."&Sign=".$sign;
+
+        $ch = curl_init() or die ( curl_error() );
+        curl_setopt( $ch, CURLOPT_URL, $url);
+        curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 360);
+        $response = curl_exec ( $ch );
+        curl_close ( $ch );
+        var_dump(json_decode($response,true));
     }
 }
