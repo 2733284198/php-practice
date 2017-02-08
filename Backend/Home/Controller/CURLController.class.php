304c8e7caa1ba3de7a2f2cddd7153fbaee08c2a5
Tinywan <756684177@qq.com>
2017-02-08 23:34:12 +0800

    Git工作流指南：Gitflow工作流

---
+++ Backend/Home/Controller/CURLController.class.php
@@ -0,0 +1,25 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Github: https://github.com/Tinywan
+ * |  Blog: http://www.cnblogs.com/Tinywan
+ * |-------------------------------------------------------------------
+ * |  Author: Tinywan
+ * |  Date: 2017/1/20
+ * |  Time: 16:25
+ * |  Mail: Overcome.wan@Gmail.com
+ * |  Created by PhpStorm.
+ * '-------------------------------------------------------------------*/
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class CURLController extends Controller
+{
+    public function index()
+    {
+        echo __METHOD__;
+        //检测扩展是否安装
+        extension_loaded('curl');
+    }
+}
