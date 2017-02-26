28ffc247f6736ee208b7920629b4eae05fbb81d6
Tinywan <756684177@qq.com>
2017-02-26 22:08:46 +0800

    132131

--- Runtime/Data/_fields/tp5.tour_tasklist.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:7:"task_id";i:1;s:10:"user_email";i:2;s:6:"status";i:3;s:11:"create_time";i:4;s:11:"update_time";s:3:"_pk";s:7:"task_id";s:5:"_type";a:5:{s:7:"task_id";s:6:"int(5)";s:10:"user_email";s:11:"varchar(40)";s:6:"status";s:6:"int(2)";s:11:"create_time";s:8:"datetime";s:11:"update_time";s:8:"datetime";}}
