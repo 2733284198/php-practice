a7acac0b9da0302f833ea0034125040248036666
Tinywan <756684177@qq.com>
2017-02-27 18:31:25 +0800

    simpleExample

---
+++ Backend/Library/Controller/MachineLearningController.class.php
@@ -0,0 +1,19 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Github: https://github.com/Tinywan
+ * |  Blog: http://www.cnblogs.com/Tinywan
+ * |-------------------------------------------------------------------
+ * |  Author: Tinywan
+ * |  Date: 2017/2/27
+ * |  Time: 18:30
+ * |  Mail: Overcome.wan@Gmail.com
+ * |  Created by PhpStorm.
+ * '-------------------------------------------------------------------*/
+
+namespace Library\Controller;
+
+
+class MachineLearningController
+{
+
+}
