78b6f7e5f5cbdef81eaa4af21ed3f9b795d6f17a
Tinywan <756684177@qq.com>
2017-03-17 17:27:52 +0800

    什么都没做

--- Backend/Home/Controller/StringController.class.php
+++ Backend/Home/Controller/StringController.class.php
@@ -25,13 +25,42 @@ class StringController extends Controller
     const F4 = 0x10; // 2^4
     const F5 = 0x20; // 2^5
 
+    public function index3()
+    {
+        //请求参数
+        $appId = 75715888;
+        $domainName = 'tinywan.amai8.com';
+        $appName = 'live';
+        $DeviceId = 801;
+        //失效以前的地址，重新获取新的推流地址
+        $DeviceInvalid = 1;
+        //签名密钥
+        $appSecret = '00205268c4e6db6bf25a04cbc0cb23e6efc7f929';
+        //拼接字符串，注意这里的字符为首字符大小写，采用驼峰命名
+        $str = "AppId" . $appId . "AppName" . $appName . "DeviceId" . $DeviceId ."DeviceInvalid" . $DeviceInvalid . "DomainName" . $domainName . $appSecret;
+        //签名串，由签名算法sha1生成
+        $sign = strtoupper(sha1($str));
+        //请求资源访问路径以及请求参数，参数名必须为大写
+        $url = "http://sewise.amai8.com/openapi/createDevicePushFlowAddress?AppId=" . $appId . "&AppName=" . $appName . "&DeviceId=" . $DeviceId . "&DeviceInvalid=" . $DeviceInvalid . "&DomainName=" . $domainName . "&Sign=" . $sign;
+        //CURL方式请求
+        $ch = curl_init() or die (curl_error());
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 360);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        //返回数据为JSON格式，进行转换为数组打印输出
+        var_dump(json_decode($response, true));
+    }
+
     public function index2()
     {
         //请求参数
         $appId = 75715888;
         $domainName = 'tinywan.amai8.com';
         $appName = 'live';
-        $DeviceId = 2801;
+        $DeviceId = 801;
         //签名密钥
         $appSecret = '00205268c4e6db6bf25a04cbc0cb23e6efc7f929';
         //拼接字符串，注意这里的字符为首字符大小写，采用驼峰命名
@@ -39,7 +68,7 @@ class StringController extends Controller
         //签名串，由签名算法sha1生成
         $sign = strtoupper(sha1($str));
         //请求资源访问路径以及请求参数，参数名必须为大写
-        $url = "http://sewise.amai8.com/openapi/createPushFlowAddress?AppId=" . $appId . "&AppName=" . $appName . "&DeviceId=" . $DeviceId . "&DomainName=" . $domainName . "&Sign=" . $sign;
+        $url = "http://sewise.amai8.com/openapi/createDevicePushFlowAddress?AppId=" . $appId . "&AppName=" . $appName . "&DeviceId=" . $DeviceId . "&DomainName=" . $domainName . "&Sign=" . $sign;
         //CURL方式请求
         $ch = curl_init() or die (curl_error());
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -56,7 +85,7 @@ class StringController extends Controller
     {
         $appId = 75715888;
 //        $domainName = 'tinywan.amai8.com';
-        $domainName = '121.40';
+        $domainName = '120.26.104.128';
         $appName = 'live';
 //        $AuthKeyStatus = 0;
         //签名密钥
@@ -135,5 +164,14 @@ spanning multiple lines
 using heredoc syntax.
 EOD;
         echo $str;
+    }
+
+    public function my_ip($dest='120.26.104.128', $port=1234)
+    {
+        $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        socket_connect($socket, $dest, $port);
+        socket_getsockname($socket, $addr, $port);
+        socket_close($socket);
+        var_dump($addr);
     }
 }
